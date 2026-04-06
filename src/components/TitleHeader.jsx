4521a968e0e0c0570fff88d83a008fe5a80175f4
krishnaGandhi11 <krishnagandhi0911@gmail.com>
2026-04-06 23:35:58 +0530

    UI clean up

--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const TitleHeader = ({title, sub}) => {
     return (
         <div className="flex flex-col items-center gap-5">
-            <div className="hero-badge">
-                <p>{sub}</p>
-            </div>
+            {sub ? (
+                <div className="hero-badge">
+                    <p>{sub}</p>
+                </div>
+            ) : null}
             <div className="font-semibold md:text-5xl text-3xl text-center">
                 {title}
             </div>
